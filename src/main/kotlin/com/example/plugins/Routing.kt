edab683eff02a2df56d2394d6e6376806a71cf2c
Alejandro Arcas Leon <alejandro.arcas.7e5@itb.cat>
2023-04-28 12:48:14 +0200

    Initial commit MainBranch

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,15 @@
+package com.example.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.resources.*
+import io.ktor.resources.*
+import io.ktor.server.resources.Resources
+import kotlinx.serialization.Serializable
+import io.ktor.server.application.*
+
+fun Application.configureRouting() {
+    install(Resources)
+    routing {
+    }
+}
---
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -0,0 +1,25 @@
+package com.example.plugins
+
+import io.ktor.server.auth.*
+import io.ktor.util.*
+import io.ktor.server.response.*
+import io.ktor.server.application.*
+
+fun Application.configureSecurity() {
+
+    authentication {
+        val myRealm = "MyRealm"
+        val usersInMyRealmToHA1: Map<String, ByteArray> = mapOf(
+            // pass="test", HA1=MD5("test:MyRealm:pass")="fb12475e62dedc5c2744d98eb73b8877"
+            "test" to hex("fb12475e62dedc5c2744d98eb73b8877")
+        )
+
+        digest("myDigestAuth") {
+            digestProvider { userName, realm ->
+                usersInMyRealmToHA1[userName]
+            }
+        }
+    }
+    routing {
+    }
+}
